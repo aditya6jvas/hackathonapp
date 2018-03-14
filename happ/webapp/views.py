7b165188b242deefaed24155569c278587ea8795
Aditya jagarlapudi <adityajagarlapudi@gmail.com>
2018-03-14 19:52:21 +0530

    push last

--- happ/webapp/views.py
+++ happ/webapp/views.py
@@ -9,17 +9,30 @@ from .models import Shop, Profile, Order, Item
 # Create your views here.
 
 def index(request):
+	def index(request):
 	if request.method == 'POST':
 		user = request.POST.get('userName')
 		email = request.POST.get('email')
 		mobile = request.POST.get('mobile')
+		product = ['Chocolates','Biscuits','Atta','Grains','Natural Oils','Toileteries','Fruits','Dry Fruits']
+		pro = []
+		a = ''
+
+		for i in range(1,9):
+			b = 'quant'+'['+str(i)+']'
+			quant = request.POST.get(b)
+			print(quant)
+			if(int(quant) > 0):
+				pro.append(product[i-1])
+
 		print(user)
 		print(email)
 		print(mobile)
+		print(quant)
+		print(pro)
 		return HttpResponse('abc')
 	else:
 		return render(request, 'index.html', {})
-
 def hi(request):
     
     queryset = Shop.objects.all()
